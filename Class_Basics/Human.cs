d953a2b191180be0d87a80727b40afbf210b6c22
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-03-01 09:00:32 -0800

    set up class to add last name

--- Class_Basics/Human.cs
+++ Class_Basics/Human.cs
@@ -3,13 +3,20 @@ namespace Class_Basics
 {
      class Human
     {
+        // create a public variable lasName of type string
+        //change the Introduction method to call the whole name
+        // create two objects with full information about themself - first and last name
+
         // member variable
        public string firstName;
+        public string lastName;
 
         // member method
         public void Introduction()
         {
-            Console.WriteLine("Hi, I'm {0}", firstName);
+            Console.WriteLine("Hi, I'm {0} {1}", firstName, lastName);
         }
+
+
     }
 }
--- Class_Basics/Program.cs
+++ Class_Basics/Program.cs
@@ -9,8 +9,16 @@ namespace Class_Basics
             //create an object of the class human
             // an "instance" of Human
             Human anna = new Human();
+            // access public variable form outside, and change it
             anna.firstName = "Anna";
+            anna.lastName = "Bell";
+            // call methods of the class
             anna.Introduction();
+
+            Human michael = new Human();
+            michael.firstName = "Michael";
+            michael.lastName = "Smith";
+            michael.Introduction();
         }
     }
 }
